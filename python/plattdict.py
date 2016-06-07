af89183a476b1d970d061496edec6f7e07f66427
elbertCmp <zhong-liang.xing@hpe.com>
2016-06-07 16:37:21 +0800

    modified plattdict.py correctly

--- python/plattdict.py
+++ python/plattdict.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
-
-
-def flatdict(dictData):
-    keyList = dictData.keys()
-    for curkey in keyList:
-        curval = dictData.pop(curkey)
+def flatdict(dictData, path, dictRst):
+    for curkey, curval in dictData.items():
         if isinstance(curval, dict):
-            subkey, subval = flatdict(curval)
-            return curkey + "/" + subkey, subval
+            if curval:
+                flatdict(curval, path + curkey + "/", dictRst)
+            else:
+                dictRst[path + curkey] = ""
         else:
-            return curkey, curval
+            dictRst[path + curkey] = curval
 
 
 def main():
-    data = {"a":{"b":{"c":{"d":"value"}}}}
-    keyList = data.keys()
-    for key in keyList:
-        val = data.pop(key)
-        if isinstance(val, dict):
-            rtnkey, rtnval = flatdict(val)
-            data[key + "/" + rtnkey] = rtnval
-                
-    print data
+    # data = {"a":{"b":{"c":{"d":"value"}}}}
+    # data = {"empty": {}}
+    data = {"name": {
+        "first": "One",
+        "last": "Drone"
+    },
+        "job": "scout",
+        "recent": {},
+        "additional": {
+            "place": {
+                "zone": "1",
+                "cell": "2"}}}
+    rstData = {}
+    flatdict(data, "", rstData)
+    print rstData
+
 
 if __name__ == "__main__":
     main()
